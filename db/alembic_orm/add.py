cc591d4cc4ce4a33f9148a77eb0eb4ab84a8ee55
Viktor Sabov <viktorszabo2002@gmail.com>
2020-12-13 15:01:54 +0200

    local setup configuration

--- db/alembic_orm/add.py
+++ db/alembic_orm/add.py
@@ -3,26 +3,26 @@ from sqlalchemy.dialects.mysql import TINYTEXT
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy.orm import sessionmaker, relationship
 
-engine = create_engine('mysql+pymysql://root:12345678@localhost:3306/pp?charset=utf8mb4', echo=True)
+engine = create_engine('mysql+pymysql://sqlalchemy:flaskpass@localhost/flask_app?charset=utf8mb4', echo=True)
 Base: DeclarativeMeta = declarative_base()
 Session = sessionmaker(bind=engine)
 
 
-class Users(Base):
+class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(255))
     username = Column(String(255))
     password_hash = Column(String(255))
-    orders = relationship('Orders', back_populates='users')
-    demands = relationship('Demands', back_populates='users')
+    orders = relationship('Order', back_populates='users')
+    demands = relationship('Demand', back_populates='users')
     """user_orders = relationship("Orders", back_populates="user")
     med_orders = relationship("Orders", back_populates="med")
     user_demands = relationship("Demands", back_populates="user")
     med_demands = relationship('Demands', back_populates="med")"""
 
 
-class Medications(Base):
+class Medication(Base):
     __tablename__ = "medications"
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
@@ -31,40 +31,38 @@ class Medications(Base):
     quantity = Column(Integer)
     in_stock = Column(Boolean)
 
-    orders = relationship('Orders', back_populates='medications')
-    demands = relationship('Demands', back_populates='medications')
+    orders = relationship('Order', back_populates='medications')
+    demands = relationship('Demand', back_populates='medications')
 
 
-class Orders(Base):
+class Order(Base):
     __tablename__ = "orders"
     id = Column(Integer, primary_key=True)
     amount = Column(Integer)
     completed = Column(Boolean)
 
     user_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship('Users', back_populates='orders')
+    users = relationship('User', back_populates='orders')
 
     med_id = Column(Integer, ForeignKey('medications.id'))
-    medications = relationship('Medications', back_populates='orders')
+    medications = relationship('Medication', back_populates='orders')
     """
     user = relationship("Users", back_populates="user_orders")
     med_id = Column(Integer, ForeignKey('medications.med_id'))
     med = relationship("Medications", back_populates="med_orders")"""
 
 
-class Demands(Base):
+class Demand(Base):
     __tablename__ = "demand"
     id = Column(Integer, primary_key=True)
     amount = Column(Integer)
 
     user_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship('Users', back_populates='demands')
+    users = relationship('User', back_populates='demands')
 
     med_id = Column(Integer, ForeignKey('medications.id'))
-    medications = relationship('Medications', back_populates='demands')
+    medications = relationship('Medication', back_populates='demands')
     """user_id = Column(Integer, ForeignKey('users.user_id'))
     user = relationship("Users", back_populates="user_demands")
     med_id = Column(Integer, ForeignKey('medications.med_id'))
     med = relationship("Medications", back_populates="med_demands")"""
-
-
